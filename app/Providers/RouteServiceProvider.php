7ec1830597f924ff6d372ca468c3f8214abe653f
Nikita Mitrofanov <cool.isaak2016@yandex.ru>
2025-01-29 13:33:56 +0500

    fix:
    unused dependencies have been removed

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -3,23 +3,10 @@
 namespace App\Providers;
 
 use App\Core\PathConfigFiles\Routes\PathConfigRouteFiles;
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
-use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    /**
-     * The path to your application's "home" route.
-     *
-     * Typically, users are redirected here after authentication.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
-
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      */
